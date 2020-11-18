c0030de31a4bfc99b6e85ab4abeef77a232f9588
Adrien Funel <adrienf@adriens-mbp.mynet>
2020-11-18 09:39:16 +0000

    Create function to build message

--- src/mail_app.py
+++ src/mail_app.py
@@ -5,23 +5,27 @@ parser = ConfigParser()
 parser.read('envvars.ini')
 
 EMAIL = parser.get('envvars', 'EMAIL_ADDRESS')
-print(EMAIL)
+PWD = parser.get('envvars', 'EMAIL_PASSWORD')
 
-def email_service():
+
+def email_service(name, url, price):
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.ehlo()
 	server.starttls()
 	server.ehlo()
-	server.login('funel.adrien@gmail.com', 'pwd')
+	server.login(EMAIL, PWD)
 
-	subject = ''
-	body = ''
+	server.sendmail(EMAIL,
+		EMAIL,
+		message_builder(name, url, price))
 
-	msg = f'Subject: {subject}\n\n{body}'
-
-	server.sendmail(
-		'',
-		'',
-		msg)
+	print('email sent')
 
 	server.quit()
+
+
+def message_builder(name, url, price):
+	subject = f'Price update on Amazon website for {name}, now down to {price.encode('utf-8')}'
+	body = f'Check the link: {url}'
+	msg = f'Subject: {subject}\n\n{body}'
+	return msg
