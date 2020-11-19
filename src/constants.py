b5dc6a60821b6cbb58affaad3a54e1492813202b
Adrien Funel <funel.adrien@gmail.com>
2020-11-19 09:50:38 +0000

    Refactor app to store constant values in config module

---
+++ src/constants.py
@@ -0,0 +1,2 @@
+URL = 'https://www.amazon.co.uk/Raspberry-Pi-ARM-Cortex-A72-Bluetooth-Micro-HDMI/dp/B07TC2BK1X/ref=sr_1_3?crid=3CHKCK8THJT9P&dchild=1&keywords=raspberry+pi+4&qid=1605655777&sprefix=raspberry%2Caps%2C158&sr=8-3'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'}
