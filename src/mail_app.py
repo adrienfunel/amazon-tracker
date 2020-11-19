ccd4c000b917eeff2b14e4ccf861a925f9d15cb6
Adrien Funel <funel.adrien@gmail.com>
2020-11-19 18:53:07 +0000

    Add Class to initiate smtp connection

--- src/mail_app.py
+++ src/mail_app.py
@@ -10,24 +10,36 @@ PWD = parser.get('envvars', 'EMAIL_PASSWORD')
 
 def email_service(name, url, price, currency):
 
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	server.login(EMAIL, PWD)
+    server = ServerConnection(EMAIL, PWD).start_connection()
 
-	server.sendmail(EMAIL,
-		EMAIL,
-		message_builder(name, url, price, currency))
+    server.sendmail(EMAIL,
+                    EMAIL,
+                    message_builder(name, url, price, currency)
+                    )
 
-	print('email sent')
-	server.quit()
+    print('email sent')
+    server.quit()
 
 
 def message_builder(name, url, price, currency):
 
-	subject = f'Price update on Amazon website for {name}, now down to {currency}{price}'
-	body = f'Check the link: {url}'
-	msg = f'Subject: {subject}\n\n{body}'
+    subject = f'Price update on Amazon website for {name}, now down to {currency}{price}'
+    body = f'Check the link: {url}'
+    msg = f'Subject: {subject}\n\n{body}'
 
-	return msg
+    return msg
+
+
+class ServerConnection:
+
+    def __init__(self, email, pwd):
+        self.email = email
+        self.pwd = pwd
+
+    def start_connection(self):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(self.email, self.pwd)
+        return server
