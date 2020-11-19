a1b65c90de364a4b80479f2fc89d4ffea6a9bc95
Adrien Funel <funel.adrien@gmail.com>
2020-11-19 09:54:28 +0000

    Refactor price value type and add logical test

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+from constants import URL, headers
 from mail_app import email_service
-
-URL = 'https://www.amazon.co.uk/Raspberry-Pi-ARM-Cortex-A72-Bluetooth-Micro-HDMI/dp/B07TC2BK1X/ref=sr_1_3?crid=3CHKCK8THJT9P&dchild=1&keywords=raspberry+pi+4&qid=1605655777&sprefix=raspberry%2Caps%2C158&sr=8-3'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'}
 
 
 def main():
@@ -15,11 +13,12 @@ def main():
 
     product_name = soup.find(id='productTitle').get_text()
     price = soup.find(id='priceblock_ourprice').get_text()
-    price_float = re.findall(r'\d+\.\d+', str(price))[0]
-    price_currency = price[0].encode('utf-8').strip()
+    price_value = re.findall(r'\d+\.\d+', price)[0]
+    # price_currency = price[0].encode('utf-8').strip()
+    price_currency = 'GBP'
 
-    if product_name and price:
-        email_service(product_name.strip(), URL, price_float, price_currency)
+    if product_name and price and float(price_value) < 50:
+        email_service(product_name.strip(), URL, price_value, price_currency)
 
 
 if __name__ == "__main__":
